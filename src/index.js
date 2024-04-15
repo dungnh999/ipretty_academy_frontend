0089dca43d747256a1e4d19a62680b10d9c8c321
DESKTOP-PDC39M3\Admin <dungnh.dev>
2024-04-15 11:16:10 +0700

    Revert "thêm cấu hình scss"
    This reverts commit 0b59e8687494f37dd881275ea14e83c44c04275c.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'academy/index.scss';
+import 'assets/index.css';
 
 import Router from 'academy/Router/Router';
 import {I18nextProvider} from 'react-i18next';
