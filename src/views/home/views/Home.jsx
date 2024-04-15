a70a17f7aa312b5f6b07005f2895c9610fdc5b04
DESKTOP-PDC39M3\Admin <dungnh.dev>
2024-04-15 12:04:15 +0700

    no message

--- src/views/home/views/Home.jsx
+++ src/views/home/views/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Banner from "academy/views/home/components/Banner";
-import CategoryCourse from "academy/views/home/components/CategoryCourse";
 
 const Home = (props) => {
     return (
         <div>
             <Banner/>
-            <CategoryCourse/>
         </div>
     )
 };
