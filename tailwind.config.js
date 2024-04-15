7d0c68c118f3f520a3955471afffdf17a0499e2d
Nguyễn Huy Dũng <dungnh.dev@gmail.com>
2024-04-15 10:42:12 +0700

    Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,15 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    fontSize: {
-      sm: '0.8rem',
-      base: '1.125rem',
-      xl: '1.25rem',
-          '2xl': '2rem',
-          '3xl': '1.953rem',
-          '4xl': '3rem',
-          '5xl': '3.052rem',
-    },
     fontFamily: {
-      sans: ["Exo", ...defaultTheme.fontFamily.sans],
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
     },
     extend: {
-      colors: {
-        bgColor: "#147B65",
-        primaryColor: "#147B65",
-        secondaryColor: "#555",
-        thirdColor: "#000",
-        whiteColor: "#fff"
-      }
+
     },
   },
   plugins: [],
