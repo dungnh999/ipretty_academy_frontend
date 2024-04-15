7d56a2d9dd8d0be604093147fb008c2255c86e7b
DESKTOP-PDC39M3\Admin <dungnh.dev>
2024-04-15 11:44:02 +0700

    Revert "Chinh sua config tailwind"
    This reverts commit 5d5109a880e721f9ce3da41ca813faf2f78a522e.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,12 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    fontSize: {
-      sm: '0.8rem',
-      base: '1.125rem',
-      xl: '1.25rem',
-      '2xl': '2rem',
-      '3xl': '1.953rem',
-      '4xl': '3rem',
-      '5xl': '3.052rem',
-    },
     fontFamily: {
-      sans: ["Exo", ...defaultTheme.fontFamily.sans],
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
     },
     extend: {
 
